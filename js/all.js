b74b7ce1fdd6506bc1db2624a1abfef642cfdf3f
AnderKovalchukHome <AnderKovalchuk@gmail.com>
2019-11-24 14:34:49 +0200

    Шапка сайта

--- js/all.js
+++ js/all.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', () => {
+    $('.main_header_mobile_icon').click(function() {
+        $('.main_header_nav').toggleClass('show');
+    });
 
+    $('.menu_main_fall_out .icon_control_down').click(function() {
+        $('.menu_main_fall_out div').toggleClass('show');
+    });
 })
